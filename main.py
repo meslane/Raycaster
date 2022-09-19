9fc1ed59e56d6520a9c86546342b057dc1c18c99
meslane <merrickslane@gmail.com>
2022-09-19 01:06:06 -0700

    add collisions

--- main.py
+++ main.py
@@ -20,13 +20,39 @@ def distance(p1, p2):
 
 class Player():
     def __init__(self):
-        self.position = [2, 4]
+        self.position = [5, 1]
         self.viewangle = 0 #0 to 360
-        self.fov = 90
-        self.rays = 120
+        self.fov = 70
+        self.rays = 90
         self.draw_distance = 8
-        self.line_resolution = 30
+        self.line_resolution = 20
         
+        self.speed = 0.02
+        
+    def move(self, keys):
+        dx = 0
+        dy = 0
+    
+        if keys[pygame.K_w]:
+            dx = self.position[0] + math.sin(math.radians(self.viewangle)) * self.speed
+            dy = self.position[1] + math.cos(math.radians(self.viewangle)) * self.speed
+        if keys[pygame.K_s]:
+            dx = self.position[0] + math.sin(math.radians(self.viewangle + 180)) * self.speed
+            dy = self.position[1] + math.cos(math.radians(self.viewangle + 180)) * self.speed
+        if keys[pygame.K_a]:
+            dx = self.position[0] + math.sin(math.radians(self.viewangle + 270)) * self.speed
+            dy = self.position[1] + math.cos(math.radians(self.viewangle + 270)) * self.speed
+        if keys[pygame.K_d]:
+            dx = self.position[0] + math.sin(math.radians(self.viewangle + 90)) * self.speed
+            dy = self.position[1] + math.cos(math.radians(self.viewangle + 90)) * self.speed
+            
+        if world[round(dy)][round(dx)] == 0:
+            self.position = [dx, dy]
+        elif world[round(dy)][round(self.position[0])] == 0:
+            self.position[1] = dy
+        elif world[round(self.position[1])][round(dx)] == 0:
+            self.position[0] = dx
+            
     def raycast(self, world, surface):
         boxwidth = surface.get_width()/self.rays + 1
         angle = self.fov/self.rays
@@ -41,10 +67,10 @@ class Player():
                 yindex = self.position[1] + (j/self.line_resolution * y)
                 xindex = self.position[0] + (j/self.line_resolution * x)
                 
-                boxheight = surface.get_height() * (1/distance(self.position, (xindex,yindex)))
                 if (0 <= round(yindex) < len(world) and 0 <= round(xindex) < len(world[0])):
                     if (world[round(yindex)][round(xindex)] == 1):
-                        pygame.draw.rect(surface, (255,0,0), 
+                        boxheight = surface.get_height() * (1/distance(self.position, (xindex,yindex)))
+                        pygame.draw.rect(surface, (128,128,128), 
                         pygame.Rect((i*surface.get_width())/self.rays, (surface.get_height()-boxheight)//2, boxwidth, boxheight))
                         break
                 else:
@@ -62,36 +88,38 @@ def main(argv):
     player = Player()
     
     run = True
+    locked = True
     fps = 0
+    mouse_speed = 2
     clock = pygame.time.Clock()
     while run:
         startloop = time.time()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-                
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    locked = False
+                    pygame.mouse.set_visible(True)
+                    pygame.event.set_grab(False)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                locked = True
+                pygame.mouse.set_visible(False)
+                pygame.event.set_grab(True)
+        
+        if locked:
+            pygame.mouse.set_pos(screen.get_width()//2,screen.get_height()//2)
+            m = pygame.mouse.get_rel()
+            if m[0] != 0 and abs(m[0]) < 300: #if the mouse moved, move camera
+                player.viewangle += math.radians(m[0] * mouse_speed)
+        
         #game code start here
         screen.fill((0,128,255))
         pygame.draw.rect(screen, (128,100,0), pygame.Rect(0, screen.get_height()//2,
                             screen.get_width(), screen.get_height()//2))
         
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
-            player.viewangle -= 1
-        if keys[pygame.K_RIGHT]:
-            player.viewangle += 1
-        if keys[pygame.K_w]:
-            player.position[0] += math.sin(math.radians(player.viewangle)) * 0.01
-            player.position[1] += math.cos(math.radians(player.viewangle)) * 0.01
-        if keys[pygame.K_s]:
-            player.position[0] += math.sin(math.radians(player.viewangle + 180)) * 0.01
-            player.position[1] += math.cos(math.radians(player.viewangle + 180)) * 0.01
-        if keys[pygame.K_a]:
-            player.position[0] += math.sin(math.radians(player.viewangle + 270)) * 0.01
-            player.position[1] += math.cos(math.radians(player.viewangle + 270)) * 0.01
-        if keys[pygame.K_d]:
-            player.position[0] += math.sin(math.radians(player.viewangle + 90)) * 0.01
-            player.position[1] += math.cos(math.radians(player.viewangle + 90)) * 0.01
+        player.move(keys)
         
         player.raycast(world, screen)
         
